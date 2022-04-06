14cf2806431d523472f6d38192ed0a1ba888d40a
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-04-06 18:17:23 -0400

    Final

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,31 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
-  return (
+function About(prop) {
+  if (prop.bio !=null) {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{prop.bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={prop.github} linkedin={prop.linkedin}/>
+      </div>
+    )
+  }
+
+  else return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={prop.github} linkedin={prop.linkedin}/>
     </div>
   );
 }
 
 export default About;
+
+// Update the About component so that:
+
+// If a prop of bio is passed to this component, it is displayed inside of a <p> tag
+// If a prop of bio is not passed down, or if bio is an empty string, don't display the <p> tag
+// Hint: you'll need to do some conditional rendering here to determine whether or not to display the <p> tag
